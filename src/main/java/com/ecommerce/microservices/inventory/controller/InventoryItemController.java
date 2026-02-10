e84a59e6c446f5ab68c1440a32548cd5e18d1621
Abhishek R <abhisheksai81@gmail.com>
2026-02-10 23:18:46 +0530

    Added x-request-id

--- src/main/java/com/ecommerce/microservices/inventory/controller/InventoryItemController.java
+++ src/main/java/com/ecommerce/microservices/inventory/controller/InventoryItemController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static com.ecommerce.microservices.inventory.config.RequestIdFilter.REQUEST_ID_HEADER;
+
 @RestController
 @RequestMapping("inventory-items")
 @RequiredArgsConstructor
@@ -21,7 +23,8 @@ public class InventoryItemController {
 
 
     @PostMapping
-    public ResponseEntity<InventoryItemResponseDto> createInventoryItem(@RequestBody @Valid InventoryItemRequestDto request) {
+    public ResponseEntity<InventoryItemResponseDto> createInventoryItem(@RequestHeader(REQUEST_ID_HEADER) String xRequestId,
+                                                                        @RequestBody @Valid InventoryItemRequestDto request) {
         InventoryItem inventoryItem = inventoryItemService.createInventoryItem(request);
         InventoryItemResponseDto response = InventoryItemResponseDto.fromEntity(inventoryItem);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
@@ -29,14 +32,15 @@ public class InventoryItemController {
 
 
     @GetMapping
-    public ResponseEntity<List<InventoryItem>> getAllInventoryItems() {
+    public ResponseEntity<List<InventoryItem>> getAllInventoryItems(@RequestHeader(REQUEST_ID_HEADER) String xRequestId) {
         List<InventoryItem> inventoryItems = inventoryItemService.getAllInventoryItems();
         return ResponseEntity.ok(inventoryItems);
     }
 
 
     @GetMapping("/{id}")
-    public ResponseEntity<InventoryItemResponseDto> getInventoryItemById(@PathVariable Long id) {
+    public ResponseEntity<InventoryItemResponseDto> getInventoryItemById(@RequestHeader(REQUEST_ID_HEADER) String xRequestId,
+                                                                         @PathVariable Long id) {
         InventoryItem inventoryItem = inventoryItemService.getInventoryItemById(id);
         InventoryItemResponseDto response = InventoryItemResponseDto.fromEntity(inventoryItem);
         return ResponseEntity.ok(response);
